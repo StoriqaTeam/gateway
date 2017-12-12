1b737db478597f4e2727b742a75dd5afd27c3e13
Alexey Karasev <karasev.alexey@gmail.com>
2017-12-12 17:28:13 +0300

    interim commit

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,133 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate juniper;
+extern crate juniper_rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+// pub mod mock;
+
+use rocket::response::content;
+use rocket::State;
+// use mock::model::Database;
+// use juniper::{EmptyMutation, RootNode};
+
+// type Schema = RootNode<'static, Database, EmptyMutation<Database>>;
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "Pong!"
 }
 
+#[get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::graphiql_source("/graphql")
+}
+
+#[get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+#[post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute(&schema, &context)
+}
+
+struct DatabasePool;
+impl DatabasePool {}
+
+struct Database;
+impl Database {}
+
+
+use juniper::{FieldResult};
+
+#[derive(GraphQLEnum)]
+enum Episode {
+    NewHope,
+    Empire,
+    Jedi,
+}
+
+#[derive(GraphQLObject)]
+#[graphql(description="A humanoid creature in the Star Wars universe")]
+struct Human {
+    id: String,
+    name: String,
+    appears_in: Vec<Episode>,
+    home_planet: String,
+}
+
+// There is also a custom derive for mapping GraphQL input objects.
+
+#[derive(GraphQLInputObject)]
+#[graphql(description="A humanoid creature in the Star Wars universe")]
+struct NewHuman {
+    name: String,
+    appears_in: Vec<Episode>,
+    home_planet: String,
+}
+
+// Now, we create our root Query and Mutation types with resolvers by using the
+// graphql_object! macro.
+// Objects can have contexts that allow accessing shared state like a database
+// pool.
+
+struct Context {
+    // Use your real database pool here.
+    pool: DatabasePool,
+}
+
+// To make our context usable by Juniper, we have to implement a marker trait.
+impl juniper::Context for Context {}
+
+struct Query;
+
+graphql_object!(Query: Context |&self| {
+
+    field apiVersion() -> &str {
+        "1.0"
+    }
+
+    // Arguments to resolvers can either be simple types or input objects.
+    // The executor is a special (optional) argument that allows accessing the context.
+    field human(&executor, id: String) -> FieldResult<Human> {
+        // Get the context from the executor.
+        let context = executor.context();
+        // Get a db connection.
+        let connection = context.pool.get_connection()?;
+        // Execute a db query.
+        // Note the use of `?` to propagate errors.
+        let human = connection.find_human(&id)?;
+        // Return the result.
+        Ok(human)
+    }
+});
+
+struct Mutation;
+
+graphql_object!(Mutation: Context |&self| {
+    field createHuman(&executor, new_human: NewHuman) -> FieldResult<Human> {
+        let db = executor.context().pool.get_connection()?;
+        let human: Human = db.insert_human(&new_human)?;
+        Ok(human)
+    }
+});
+
+// A root schema consists of a query and a mutation.
+// Request queries can be executed against a RootNode.
+type Schema = juniper::RootNode<'static, Query, Mutation>;
+
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![ping, graphiql, get_graphql_handler, post_graphql_handler]).launch();
 }
