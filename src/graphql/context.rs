c866df4e817be70a46e9e1aaad45ee406137f538
serejkaaa512 <5125402@mail.ru>
2017-12-14 13:53:31 +0300

    changed file structure

--- src/graphql/context.rs
+++
@@ -1,7 +0,0 @@
-extern crate juniper;
-
-// Graphql context
-pub struct Context;
-
-// To make our context usable by Juniper, we have to implement a marker trait.
-impl juniper::Context for Context {}
