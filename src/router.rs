852b5294603a6ccb67508f11f5f88e3f3190d73a
AlexeyKarasev <karasev.alexey@gmail.com>
2017-12-17 13:06:02 +0300

    added some docs

--- src/router.rs
+++ src/router.rs
@@ -2,15 +2,17 @@ use regex::{Regex};
 
 type ParamsConverter = Fn(Vec<&str>) -> Option<Route>;
 
+/// Router class maps regex to type-safe list of routes, defined by `enum Route`
 pub struct Router {
     regex_and_converters: Vec<(Regex, Box<ParamsConverter>)>,
 }
 
+/// List of all routes with params for the app
 #[derive(Clone)]
 pub enum Route {
     Root,
     Graphql,
-    Users(i32)
+    Users(i32) // this is for demo purposes, not needed for graphql
 }
 
 impl Router {
@@ -18,6 +20,7 @@ impl Router {
         Router { regex_and_converters: Vec::new() }
     }
 
+    /// Adds mapping between regex and route
     pub fn add_route(&mut self, regex_pattern: &str, route: Route) -> &Self {
         self.add_route_with_params(regex_pattern, move |_| {
             Some(route.clone())
@@ -25,7 +28,24 @@ impl Router {
         self
     }
 
-
+    /// Adds mapping between regex and route with params
+    /// converter is a function with argument being a set of regex matches (strings) for route params in regex
+    /// this is needed if you want to convert params from strings to int or some other types
+    ///
+    /// #Example
+    /// 
+    /// ```
+    /// enum Route {
+    ///     Users(i32)
+    /// }
+    ///
+    /// let router = Router::new();
+    /// router.add_route_with_params(r"^/users/(\d+)$", |params| {
+    ///     params.get(0)
+    ///        .and_then(|string_id| string_id.parse::<i32>().ok())
+    ///        .map(|user_id| Route::Users(user_id))
+    /// });
+    /// ```
     pub fn add_route_with_params<F>(&mut self, regex_pattern: &str, converter: F) -> &Self 
         where F: Fn(Vec<&str>) -> Option<Route> + 'static {
         let regex = Regex::new(regex_pattern).unwrap();
@@ -33,6 +53,8 @@ impl Router {
         self
     }
 
+    /// Tests string router for matches
+    /// Returns Some(route) if there's a match
     pub fn test(&self, route: &str) -> Option<Route> {
         self.regex_and_converters.iter().fold(None, |acc, ref regex_and_converter| {
             if acc.is_some() { return acc }
