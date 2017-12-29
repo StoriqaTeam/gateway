1309bce79df0f0d4edf23da0dbfd64059fad3bd6
serejkaaa512 <5125402@mail.ru>
2017-12-29 10:04:12 +0300

    added env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 extern crate env_logger;
-#[macro_use]
 extern crate log;
-use env_logger::{LogBuilder, LogTarget};
+use env_logger::LogBuilder;
 extern crate gateway_lib;
 
 
 
 fn main() {
-    LogBuilder::new().target(LogTarget::Stdout).init().unwrap();
+    LogBuilder::new().init().unwrap();
 
     let config = gateway_lib::config::Config::new()
         .expect("Can't load gateway configs. Please check your /config folder.");
