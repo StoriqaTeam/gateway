889a21e02c140c2c9c2262b442afd23952af55b8
serejkaaa512 <5125402@mail.ru>
2018-01-04 12:48:58 +0300

    fixes after revision 2

--- src/graphql/schema.rs
+++ src/graphql/schema.rs
@@ -19,44 +19,67 @@ pub fn create() -> Schema {
 #[derive(GraphQLObject, Deserialize, Debug)]
 #[graphql(description = "JWT Token")]
 pub struct JWT {
-    #[graphql(description = "Token")]
+    #[graphql(description = "Token")] 
     pub token: String,
 }
 
-#[derive(GraphQLObject, Deserialize, Debug)]
-#[graphql(description = "User's profile")]
+#[derive(Deserialize, Debug)]
 pub struct User {
-    #[graphql(description = "Unique id")]
     pub id: i32,
-    #[graphql(description = "Email")]
     pub email: String,
-    #[graphql(name = "isActive", description = "If the user was disabled (deleted), isActive is false")]
     pub is_active: bool,
 }
 
 enum Node {
     User(User),
+}
+
+graphql_interface!(Node: () as "Node" |&self| {
+    description: "The Node interface contains a single field, 
+        id, which is a ID!. The node root field takes a single argument, 
+        a ID!, and returns a Node. These two work in concert to allow refetching."
     
-}
-
-#[derive(GraphQLEnum)]
-enum Provider {
-  Google,
-  Facebook
-}
-
-graphql_interface!(Node: () |&self| {
     field id() -> &i32 {
         match *self {
-            Node::User (User { ref id, .. })  => id,
+            Node::User(User { ref id, .. })  => id,
         }
     }
 
     instance_resolvers: |_| {
-        &User => match *self { Node::User(ref user) => Some(user), _ => None },
+        &User => match *self { Node::User(ref h) => Some(h), _ => None },
     }
 });
 
+
+graphql_object!(User: () as "User" |&self| {
+    description: "User's profile."
+
+    interfaces: [&Node]
+
+    field id() -> &i32 as "Unique id"{
+        &self.id
+    }
+
+    field email() -> String as "Email" {
+        self.email.clone()
+    }
+
+    field isActive() -> bool as "If the user was disabled (deleted), isActive is false" {
+        self.is_active
+    }
+
+});
+
+
+
+#[derive(GraphQLEnum)]
+#[graphql(name = "Provider", description = "Token providers")]
+enum Provider {
+    #[graphql(description = "Google")] 
+    Google,
+    #[graphql(description = "Facebook")] 
+    Facebook,
+}
 
 graphql_object!(Query: Context |&self| {
 
@@ -109,10 +132,36 @@ graphql_object!(Query: Context |&self| {
             .or_else(|err| Err(err.to_graphql()))
             .wait()
     }
+
+    field node(&executor, id: i32 as "Id of a user.") -> FieldResult<Option<Node>> as "Fetches graphql interface node by id. Remote."  {
+        let context = executor.context();
+        let url = format!("{}/users/{}", context.config.users_microservice.url.clone(), id);
+        context.http_client.request::<User>(Method::Get, url, None)
+            .map(|res| Some(Node::User(res)))
+            .or_else(|err| Err(err.to_graphql()))
+            .wait()
+    }
+
 });
 
-
 graphql_object!(Mutation: Context |&self| {
+     
+    description: "Top level mutation.
+
+    Codes:
+    - 100 - microservice responded,
+    but with error http status. In this case `details` is guaranteed
+    to have `status` field with http status and
+    probably some additional details.
+
+    - 200 - there was a network error while connecting to microservice.
+
+    - 300 - there was a parse error - that usually means that
+    graphql couldn't parse api json response
+    (probably because of mismatching types on graphql and microservice)
+    or api url parse failed.
+
+    - 400 - Unknown error."
 
     field createUser(&executor, email: String as "Email of a user.", password: String as "Password of a user.") -> FieldResult<User> as "Creates new user." {
         let context = executor.context();
@@ -157,7 +206,7 @@ graphql_object!(Mutation: Context |&self| {
             .wait()
     }
 
-    field getJWTByProvider(&executor, provider: Provider, token: String as "Providers token.") -> FieldResult<JWT> as "Get JWT Token by Google oauth." {
+    field getJWTByProvider(&executor, provider: Provider as "Token provider", token: String as "Token.") -> FieldResult<JWT> as "Get JWT Token from token provider." {
         let context = executor.context();
         let provider = match provider {
             Provider::Facebook => "facebook",
@@ -173,4 +222,3 @@ graphql_object!(Mutation: Context |&self| {
     }
 
 });
-
