07d71951f3c362acf4b2c7930f2a0c539d494c69
serejkaaa512 <5125402@mail.ru>
2018-02-26 18:11:36 +0300

    renamed id to key in graphql

--- src/graphql/models/currency.rs
+++ src/graphql/models/currency.rs
@@ -1,5 +1,6 @@
 #[derive(GraphQLObject, Serialize, Deserialize, Debug)]
 pub struct Currency {
+    #[graphql(name="key")]
     pub id: i32,
     pub name: String,
 }
--- src/graphql/models/language.rs
+++ src/graphql/models/language.rs
@@ -1,5 +1,6 @@
 #[derive(GraphQLObject, Serialize, Deserialize, Debug)]
 pub struct Language {
+    #[graphql(name="key")]
     pub id: i32,
     pub name: String,
 }
