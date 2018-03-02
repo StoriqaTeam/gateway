c07e9222d68c7f8b34bb960739e4262ff7d8ffd0
serejkaaa512 <5125402@mail.ru>
2018-03-02 15:48:39 +0300

    products auto complete

--- src/graphql/schema/query.rs
+++ src/graphql/schema/query.rs
@@ -226,6 +226,44 @@ graphql_object!(Query: Context |&self| {
             .wait()
     }
 
+    field products_name_auto_complete(&executor, first = None : Option<i32> as "First edges", after = None : Option<i32>  as "Offset form begining", search_term : SearchProductInput as "Search product input") -> FieldResult<Connection<String>> as "Finds products full name by part of the name." {
+        let context = executor.context();
+
+        let offset = after.unwrap_or_default();
+
+        let records_limit = context.config.gateway.records_limit;
+        let count = cmp::min(first.unwrap_or(records_limit as i32), records_limit as i32);
+
+        let body = serde_json::to_string(&search_term)?;
+
+        let url = format!("{}/{}/auto_complete?count={}&offset={}",
+            context.config.service_url(Service::Stores),
+            Model::Product.to_url(),
+            count + 1,
+            offset
+            );
+
+        context.http_client.request_with_auth_header::<Vec<String>>(Method::Get, url, Some(body), context.user.as_ref().map(|t| t.to_string()))
+            .or_else(|err| Err(err.into_graphql()))
+            .map (|full_names| {
+                let mut full_name_edges: Vec<Edge<String>> =  vec![];
+                for i in 0..full_names.len() {
+                    let edge = Edge::new(
+                            juniper::ID::from( (i as i32 + offset).to_string()),
+                            full_names[i].clone()
+                        );
+                    full_name_edges.push(edge);
+                }
+                let has_next_page = full_name_edges.len() as i32 == count + 1;
+                if has_next_page {
+                    full_name_edges.pop();
+                };
+                let has_previous_page = true;
+                let page_info = PageInfo {has_next_page: has_next_page, has_previous_page: has_previous_page};
+                Connection::new(full_name_edges, page_info)
+            })
+            .wait()
+    }
 
     field languages(&executor) -> FieldResult<Vec<LanguageGraphQl>> as "Fetches languages." {
         Ok(Language::as_vec())
