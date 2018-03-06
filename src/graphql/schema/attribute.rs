aa6b64dfd6fdc9d754b3745a2aa759d6815dd5fe
serejkaaa512 <5125402@mail.ru>
2018-03-06 16:09:57 +0300

    updated libstqbackend

--- src/graphql/schema/attribute.rs
+++ src/graphql/schema/attribute.rs
@@ -1,12 +1,18 @@
 //! File containing product object of graphql schema
 use juniper::ID as GraphqlID;
 use stq_static_resources::Translation;
+use stq_routes::model::Model;
+use stq_routes::service::Service;
 
 use graphql::context::Context;
 use graphql::models::*;
 
 graphql_object!(Attribute: Context as "Attribute" |&self| {
     description: "Attribute's info."
+
+    field id() -> GraphqlID as "Unique id"{
+        ID::new(Service::Stores, Model::Attribute, self.id).to_string().into()
+    }
 
     field raw_id() -> GraphqlID as "Unique id"{
         self.id.to_string().into()
