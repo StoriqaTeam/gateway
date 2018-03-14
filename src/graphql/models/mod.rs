0b53634f59539ddb0d89b833ec55052f062ddda1
Sergey Efremov <5125402@mail.ru>
2018-03-14 16:38:38 +0300

    added category attributes endpoints

--- src/graphql/models/mod.rs
+++ src/graphql/models/mod.rs
@@ -25,3 +25,16 @@ pub use self::provider::Provider;
 pub use self::jwt::{CreateJWTEmailInput, CreateJWTProviderInput, ProviderOauth, UserStatus, JWT};
 pub use self::user_role::{NewUserRole, Role, UserRole};
 pub use self::category::*;
+
+use graphql::context::Context;
+
+pub struct Mock;
+
+graphql_object!(Mock: Context as "Mock" |&self| {
+    description: "Mock field."
+
+    field mock() -> String as "Mock"{
+        "Mock".to_string()
+    }
+
+});
