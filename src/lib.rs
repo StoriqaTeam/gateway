8d37fd522cffbd24006d9c50c31a58a03b793261
Sergey Efremov <5125402@mail.ru>
2018-04-03 15:32:39 +0300

    update juniper hyper logger chrono libs

--- src/lib.rs
+++ src/lib.rs
@@ -12,29 +12,49 @@ extern crate regex;
 extern crate serde;
 extern crate serde_json;
 extern crate tokio_core;
-
 #[macro_use]
 extern crate serde_derive;
-
 #[macro_use]
 extern crate log;
-
 extern crate base64;
 extern crate jsonwebtoken;
+extern crate env_logger;
+extern crate chrono;
 
-pub mod config;
-mod graphql;
-pub mod http;
+use std::sync::Arc;
+use std::env;
+use std::io::Write;
 
 use futures::stream::Stream;
 use tokio_core::reactor::Core;
-use std::sync::Arc;
+use chrono::prelude::*;
+use env_logger::Builder as LogBuilder;
+use log::LevelFilter as LogLevelFilter;
 
 use stq_http::client::Client as HttpClient;
 
 use config::Config;
 
+pub mod config;
+pub mod graphql;
+pub mod http;
+
 pub fn start(config: Config) {
+    let mut builder = LogBuilder::new();
+    builder
+        .format(|formatter, record| {
+            let now = Utc::now();
+            writeln!(formatter, "{} - {} - {}", now.to_rfc3339(), record.level(), record.args())
+        })
+        .filter(None, LogLevelFilter::Info);
+
+    if env::var("RUST_LOG").is_ok() {
+        builder.parse(&env::var("RUST_LOG").unwrap());
+    }
+
+    // Prepare logger
+    builder.init();
+
     let config = Arc::new(config);
 
     let mut core = Core::new().expect("Unexpected error creating main event loop");
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
-extern crate env_logger;
-extern crate log;
-use env_logger::LogBuilder;
 extern crate gateway_lib;
 
 fn main() {
-    LogBuilder::new().init().unwrap();
-
     let config = gateway_lib::config::Config::new().expect("Can't load gateway configs. Please check your /config folder.");
     gateway_lib::start(config);
 }
