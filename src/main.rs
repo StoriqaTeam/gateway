6cf96015cf75b0dd170385982aa4c2a9f0bbb78a
Serejka <5125402@mail.ru>
2018-07-23 10:53:47 +0300

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 extern crate gateway_lib;
+extern crate stq_logging;
 
 fn main() {
     let config = gateway_lib::config::Config::new().expect("Can't load gateway configs. Please check your /config folder.");
+
+    // Prepare logger
+    stq_logging::init(config.graylog.as_ref());
+
     gateway_lib::start(config);
 }
