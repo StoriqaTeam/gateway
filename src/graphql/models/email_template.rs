2b5830003cc3a43542782b8e2684d21e8d606d62
Alexander Andreev <andreevlex.as@gmail.com>
2018-08-22 11:26:11 +0300

    added methods for work email templates (#252)
    * added get email templates
    
    * added mitations for email temlates
    
    * add client_mutation_id

---
+++ src/graphql/models/email_template.rs
@@ -0,0 +1,20 @@
+pub struct EmailTemplate;
+
+#[derive(GraphQLInputObject, Serialize, Debug, Clone, PartialEq)]
+#[graphql(description = "Update email template input object")]
+pub struct EmailTemplateInput {
+    #[graphql(description = "Client mutation id.")]
+    #[serde(skip_serializing)]
+    pub client_mutation_id: String,
+    #[graphql(description = "Data of a email template.")]
+    pub data: String,
+}
+
+impl EmailTemplateInput {
+    pub fn is_none(&self) -> bool {
+        Self {
+            client_mutation_id: self.client_mutation_id.clone(),
+            data: String::default(),
+        } == self.clone()
+    }
+}
