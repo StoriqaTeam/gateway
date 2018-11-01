0fb131f391fb2a129b1f478bf88a689f2cc71815
Semyon Ivanov <semyon95@gmail.com>
2018-11-01 18:46:40 +0300

    add new Base64 id to AvailablePackagesForUser

--- src/graphql/models/delivery.rs
+++ src/graphql/models/delivery.rs
@@ -257,6 +257,7 @@ impl From<Vec<AvailablePackages>> for AvailablePackagesOutput {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AvailablePackageForUser {
     pub id: CompanyPackageId,
+    pub shipping_id: i32,
     pub name: String,
     pub logo: String,
     pub price: Option<ProductPrice>,
