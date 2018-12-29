9afe29d85176f01347e68d4c38e93fa392caa5ac
Alexander Andreev <andreevlex.as@gmail.com>
2018-12-29 14:01:17 +0300

    Improve SagaService and used in Context

--- src/graphql/microservice/saga.rs
+++ src/graphql/microservice/saga.rs
@@ -10,27 +10,29 @@ use graphql::context::Context;
 use graphql::models::*;
 
 pub trait SagaService {
-    fn upsert_shipping(&self, context: &Context, base_product_id: BaseProductId, shipping: NewShipping) -> FieldResult<Shipping>;
+    fn upsert_shipping(&self, base_product_id: BaseProductId, shipping: NewShipping) -> FieldResult<Shipping>;
 }
 
-pub struct SagaServiceImpl;
+pub struct SagaServiceImpl<'ctx> {
+    context: &'ctx Context,
+}
 
-impl SagaServiceImpl {
-    pub fn new() -> Self {
-        SagaServiceImpl {}
+impl<'ctx> SagaServiceImpl<'ctx> {
+    pub fn new(context: &'ctx Context) -> Self {
+        SagaServiceImpl { context }
     }
 }
 
-impl SagaService for SagaServiceImpl {
-    fn upsert_shipping(&self, context: &Context, base_product_id: BaseProductId, shipping: NewShipping) -> FieldResult<Shipping> {
+impl<'ctx> SagaService for SagaServiceImpl<'ctx> {
+    fn upsert_shipping(&self, base_product_id: BaseProductId, shipping: NewShipping) -> FieldResult<Shipping> {
         let url = format!(
             "{}/{}/{}/upsert-shipping",
-            context.config.saga_microservice.url,
+            self.context.config.saga_microservice.url,
             Model::BaseProduct.to_url(),
             base_product_id
         );
 
         let body: String = serde_json::to_string(&shipping)?;
-        context.request::<Shipping>(Method::Post, url, Some(body)).wait()
+        self.context.request::<Shipping>(Method::Post, url, Some(body)).wait()
     }
 }
